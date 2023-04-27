5f26337d500c8d2752ab3bc8aa225490622fbf55
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2023-04-27 09:21:33 +0300

    add interfaces for basics project

--- basics/main.go
+++ basics/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"main/conditions"
+	"main/interfaces"
 )
 
 func main() {
 	fmt.Println("Hello World")
 
 	conditions.PrintConditions()
+
+	interfaces.PrintInterface()
 }
